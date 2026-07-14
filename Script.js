34a344bec9ee46c0f51b85c1e36a03acbcee322d
carenchemutai384-sudo <carenchemutai384@gmail.com>
2026-07-14 21:02:08 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,12 @@
+document.getElementById("askBtn").addEventListener("click", function () {
 
+    const question = document.getElementById("question").value;
+
+    if (question.trim() === "") {
+        alert("Please enter a KCSE question.");
+        return;
+    }
+
+    alert("Coming soon!\n\nAI will answer:\n\n" + question);
+
+});

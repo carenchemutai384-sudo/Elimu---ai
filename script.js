d025993c3417b3e377697b7a45d10d6b0e9ae7c1
carenchemutai384-sudo <carenchemutai384@gmail.com>
2026-07-15 10:02:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ async function askAI() {
     try {
 
         const response = await fetch(
-            "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=" + apiKey,
+            "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=" + apiKey,
             {
                 method: "POST",
                 headers: {
